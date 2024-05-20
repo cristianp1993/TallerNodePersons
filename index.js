ad3f08076940883c078942f8ba64b5631e9b7793
cristian <cristian.piedrahita88@gmail.com>
2024-05-19 23:47:38 -0500

    Taller node

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config();
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+// Middleware
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Rutas
+const personsRouter = require('./routes/persons');
+app.use('/persons', personsRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
